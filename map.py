1d0a767628810835521c5b1c57135bbd4d53ed7c
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-01-18 16:52:33 +0100

    Ajout de la v1 pour la map

---
+++ map.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+File : map.py
+
+Gestion de la map.
+"""
+
+from random import choice, random
+
+class Map:
+    """
+    Attributs :
+
+        map => map elle même
+        longueur => longueur
+        largeur => largeur
+    """
+
+    types = {
+        'Trou' : 2 ,
+        'Mur' : 12,
+        'Eau' : 4,
+        'Terre' : 20,
+    }
+
+    def __init__(self, longueur, largeur):
+        """
+        constructeur
+        """
+
+        self.longueur = longueur
+        self.largeur = largeur
+
+        # générer la map elle même.
+        self.map = [[choice(self.generer_liste_types()) for _ in range(self.largeur)] for __ in range(self.longueur)]
+
+    def generer_liste_types(self):
+        """ genere une liste des possibles pour les cases """
+        types = []
+        for t in Map.types.keys():
+            for n in range(Map.types[t]):
+                types.append(t)
+
+        return types
+
