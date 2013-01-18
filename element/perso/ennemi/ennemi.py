c7aec0e6033a98db3b05cbe24531fa5d97128e15
ejeFi2in3F <thomas.huba@gmail.com>
2013-01-18 19:53:07 +0100

    add Ennemi Init

--- element/perso/ennemi/ennemi.py
+++ element/perso/ennemi/ennemi.py
@@ -7,6 +7,22 @@ import settings
 #########################
 # ENNEMI (CLASS)        #
 #########################
+class Ennemi(Perso):
+	"""
+	Classe de l'ennemi, dérivant de Perso.
+	"""
 
+	def __init__(self, nom, carSec = 10,
+		   carIhm = 10, carKernel = 10, carHard = 10):
+	"""
+	Initialise l'ennemi, selon Element 
+	Argument :
+		- nom (string)
+		- carac securité (entier, 10 PD)
+                - carac ihm (entier, 10 PD)
+                - carac kernel (entier, 10 PD)
+                - carac hard (entier, 10 PD)
+        """
+	Perso.__init__(self, nom, carSec, carIhm, carKernel, carHard);
 
 
